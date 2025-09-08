ce841af2a4a4db63b415f23730cfe28614b5112f
Abruino <Dansku.personal@fedora.lt>
2025-09-08 10:26:47 +0300

    Created new branch and push added shitcode

--- sketch_sep8a.ino
+++ sketch_sep8a.ino
@@ -1,7 +1,11 @@
 void setup() {
  //HGSHSFHHDGjhfbfdfgtghff fhrft dyh rfd chf f yr yrf d y
+ uint8_t balls;
+ bool isJorniGoated=false;
 }
 
 void loop() {
+  if (isJorniGoated)balls=1;
  delay(250); //Ok :)
+ if (balls=1)print("Amazing");
 }
